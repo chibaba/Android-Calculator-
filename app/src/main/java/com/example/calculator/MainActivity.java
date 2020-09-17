25ac605ea8408717fa7a2187096c190be7c2de62
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-17 13:00:09 +0100

    wip

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -21,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
 
 
 
